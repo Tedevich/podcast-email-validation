08a55084264891d4cbfdf4a017ba803c0ae978a0
Tedevich <123385803+Tedevich@users.noreply.github.com>
2023-03-26 20:40:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+
+    if(email.value ===''){
+        error.innerHTML='Oops! Please add your email';
+        
+    }else if(!re.test(email.value)){
+        error.innerHTML='Oops! Please Check your email';
+
+    }
+    
+    
+
+}
+
+)
